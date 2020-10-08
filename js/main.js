063b3665751b7895f1f6966fb59d28665922d144
gga01075 <72422020+gga01075@users.noreply.github.com>
2020-10-08 17:37:05 +0900

    최소높이 지정, alt수정
    2020.10.08

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     //index 스크롤이벤트      
     $(window).on('scroll',function(){ 
-        // 대회소개
+        // index 리그전
         if($(window).scrollTop() > $(".cnt2").offset().top - 600){
             gsap.to("#league h3", {delay:0.2,marginLeft:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
             gsap.to("#league>ul>li:nth-of-type(1)", {delay:0.4,marginLeft:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
@@ -9,7 +9,7 @@ $(function(){
             gsap.to("#league>ul>li:nth-of-type(3)", {delay:0.8,marginLeft:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
             gsap.to("#league>ul>li:nth-of-type(4)", {delay:1.0,marginLeft:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
             gsap.to("#league>.read_more", {delay:0.8,marginLeft:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
-
+        //index 패치노트
             gsap.to("#patchnote h3", {delay:0.2,marginLeft:-50, opacity:1, duration: 0.5, ease:Power3.easeOut});
             gsap.to("#patchnote>ul>li:nth-of-type(1)", {delay:0.4,marginLeft:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
             gsap.to("#patchnote>ul>li:nth-of-type(2)", {delay:0.6,marginLeft:70, opacity:1, duration: 0.5, ease:Power3.easeOut});
@@ -17,7 +17,7 @@ $(function(){
             gsap.to("#patchnote>ul>li:nth-of-type(4)", {delay:1.0,marginLeft:210, opacity:1, duration: 0.5, ease:Power3.easeOut});
             gsap.to("#patchnote>.read_more", {delay:0.8,marginLeft:270, opacity:1, duration: 0.5, ease:Power3.easeOut});
         }
-
+        //index 신상품
         if($(window).scrollTop() > $(".cnt4").offset().top - 650){
             gsap.to("#storeNew h2", {delay:0.2,marginLeft:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
             gsap.to("#storeNew .area >p", {delay:0.2,marginLeft:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
@@ -28,6 +28,14 @@ $(function(){
             gsap.to("#storeNew .store_list li:nth-of-type(-n+2)", {delay:0,marginTop:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
             gsap.to("#storeNew .store_list li:nth-of-type(n+2)", {delay:0.2,marginTop:0, opacity:1, duration: 0.5, ease:Power3.easeOut});
         }
+
+        //index 신상품 목록 hover
+        $('#storeNew .store_list li a').on('focusin',function(){
+            $(this).parent('li').addClass('on');
+        });
+        $('#storeNew .store_list li a').on('focusout',function(){
+            $(this).parent('li').removeClass('on');
+        });
 
 
 
